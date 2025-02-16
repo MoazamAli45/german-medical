de5ceb118cd52e45a3fe996662fa2bd4f0cbb157
Syed Moazam Ali <107930498+MoazamAli45@users.noreply.github.com>
2025-02-16 19:21:05 +0500

    Improve sidebar responsiveness by adjusting overflow behavior in RightSidebar component

--- src/components/details/medical-sidebar.tsx
+++ src/components/details/medical-sidebar.tsx
@@ -158,7 +158,7 @@ export function RightSidebar({ id }: { id: string }) {
         </div>
       ) : (
         <div
-          className={`fixed lg:absolute right-0 top-0 h-full w-[300px] sm:w-[360px] bg-white p-4 overflow-y-auto border-gray-200 scrollbar-hide transition-transform duration-300 ease-in-out ${
+          className={`fixed lg:absolute right-0 top-0 h-full w-[300px] sm:w-[360px] bg-white p-4  border-gray-200 overflow-auto lg:overflow-visible scrollbar-hide transition-transform duration-300 ease-in-out ${
             isOpen ? "translate-x-0" : "translate-x-full"
           } lg:translate-x-0`}
         >
